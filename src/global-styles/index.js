d71f983f4478e67c720922cc7b0e3c8dcd62982b
Polina Kozich <polina.kozich@pandadoc.com>
2021-03-04 12:35:37 +0300

    Added style variables, assets

--- src/global-styles/index.js
+++ src/global-styles/index.js
@@ -1,18 +1,34 @@
 import { createGlobalStyle } from 'styled-components'
 
+import Mulish from '../assets/fonts/Mulish/mulish-regular.woff2'
+
+import * as vars from './variables.js';
+
 export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Mulish';
+    src: url(${Mulish}) format('woff2');
+    font-weight: 400;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box; // 1
+  }
+
   body {
-    font-family: -apple-system, BlinkMacSystemFont, \"Segoe UI\", Roboto, Helvetica, Arial, sans-serif, \"Apple Color Emoji\", \"Segoe UI Emoji\", \"Segoe UI Symbol\";
-    margin: 0; 
+    font-family: ${vars.fontFamily.base};
+    margin: 0;
     font-weight: normal;
     line-height: 1.2;
-    color: black;
-    text-align: left; 
-    background-color: #fff; 
-    font-size: 14px;
+    color: ${vars.colors.brandBlack};
+    font-size: ${vars.fontSize.baseMobile};
+    text-align: left;
+    background-color: ${vars.colors.white};
 
-    @media (min-width: 600px) {
-      font-size: 16px;
+    @media (min-width: ${vars.breakpoints.tablet}) {
+      font-size: ${vars.fontSize.base};
     }
   }
 `
---
+++ src/global-styles/variables.js
@@ -0,0 +1,26 @@
+export const colors = {
+    grey: "#70707b",
+    brand: "#0080ff",
+    brandBlack: "#10111d",
+    black: "#000",
+    white: "#fff"
+}
+
+export const fontFamily = {
+    base: "Mulish, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'"
+}
+
+export const fontSize = {
+    base: "16px",
+    baseMobile: "14px"
+}
+
+export const breakpoints = {
+    tablet: "600px"
+}
+
+export const units = {
+    ss: ".25rem",
+    sm: ".5rem",
+    sl: "1rem"
+}
